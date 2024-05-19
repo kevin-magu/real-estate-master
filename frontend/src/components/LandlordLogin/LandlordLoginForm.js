5443d35d6cdf45a78997c9cf87cb188548d61c5c
kevin-magu <wanjaukevinmagu@gmail.com>
2024-05-19 19:51:15 +0300

    real-estate

--- frontend/src/components/LandlordLogin/LandlordLoginForm.js
+++ frontend/src/components/LandlordLogin/LandlordLoginForm.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import '../../styles/LandlordLogin.css'
+
 
 function LandlordLoginForm() {
   return (
-    <div>
-      
+    <div className='register-landlord-wrapper'>
+      <form>
+          
+          <label htmlFor=''>Email: </label>
+          <input id='email' type="text" />
+         
+          <label htmlFor='password'>Password: </label>
+          <input id='password' type="text" />
+    
+          <button>Login</button>
+      </form>
     </div>
   )
 }

3dd7744fb9e031c512b8110b1be775e6423179d0
kevin-magu <wanjaukevinmagu@gmail.com>
2024-05-20 23:04:15 +0300

    real-estate

--- frontend/src/components/LandlordLogin/LandlordLoginForm.js
+++ frontend/src/components/LandlordLogin/LandlordLoginForm.js
@@ -1,19 +1,49 @@
-import React from 'react'
+import React,{useState} from 'react'
 import '../../styles/LandlordLogin.css'
+function LandlordLoginForm({onLogin}) {
 
+const [email, setEmail] = useState("")
+const [password, setPassword] = useState("")
 
-function LandlordLoginForm() {
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const data = {
+    email,
+    password
+  };
+
+  try{
+    const response = await fetch("http://127.0.0.1/login", {
+    method: "POST",
+    headers:{
+      "Content-Type": "application/json"
+    }, 
+    body: JSON.stringify(data)
+});
+    if(!response.ok){
+      const result = await response.json();
+      alert(result.message)
+    }else{
+      const result = await response.json()
+      onLogin(result)
+    }
+
+  }catch(error){
+    console.error("error loggin in", error.message)
+  }
+}
+
   return (
     <div className='register-landlord-wrapper'>
-      <form>
-          
+      <form onSubmit={handleSubmit}>
           <label htmlFor=''>Email: </label>
-          <input id='email' type="text" />
+          <input id='email' type="text" value={email} onChange={(e) =>setEmail(e.target.value)}/>
          
           <label htmlFor='password'>Password: </label>
-          <input id='password' type="text" />
+          <input id='password' type="text" value={email} onChange={(e) => setPassword(e.target.value)}/>
     
-          <button>Login</button>
+          <button type='submit'>Login</button>
       </form>
     </div>
   )
